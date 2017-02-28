5706e5e9a54d61302e083761c954d09580937ea8
JSco <jackmichaelscotty@gmail.com>
2017-02-27 23:14:59 -0600

    added a computed observable

--- js/app.js
+++ js/app.js
@@ -7,6 +7,23 @@ var ViewModel = function() {
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	}
+    
+    this.level = ko.computed(function() {
+        var level = 'New-Born';
+        if (this.clickCount() > 5) {
+            level = 'Infant';
+        }
+        if (this.clickCount() > 10) {
+            level = 'Toddler';
+        }
+        if (this.clickCount() > 15) {
+            level = 'Crazy Person';
+        }
+        if (this.clickCount() > 30) {
+            level = 'You don\'t want to know... stop clicking';
+        }
+        return level;
+    }, this);
 }
 
 ko.applyBindings(new ViewModel());
