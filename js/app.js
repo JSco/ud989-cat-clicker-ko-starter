916adf6213190f55912441f434e5ffdea0bbd158
JSco <jackmichaelscotty@gmail.com>
2017-02-27 23:36:02 -0600

    added a for-each loop with knockout control flow

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-var ViewModel = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttibution = ko.observable('https://www.flickr.com/photos/big');
+var ViewModel = function () {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttibution = ko.observable('https://www.flickr.com/photos/big');
 
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
-	}
-    
-    this.level = ko.computed(function() {
+    this.incrementCounter = function () {
+        this.clickCount(this.clickCount() + 1);
+    }
+
+    this.level = ko.computed(function () {
         var level = 'New-Born';
         if (this.clickCount() > 5) {
             level = 'Infant';
@@ -24,6 +24,13 @@ var ViewModel = function() {
         }
         return level;
     }, this);
+
+    this.nicknames = ko.observableArray([
+        {nickname: 'tabby cat'},
+        {nickname: 'spike'},
+        {nickname: 'mr. mcgillicuddy'},
+        {nickname: 'joe'}
+    ]);
 }
 
 ko.applyBindings(new ViewModel());
